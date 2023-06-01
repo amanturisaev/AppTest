5ed43924aa6f75703452ab3bae830aa25773aa47
Amantur Isaev <eunoianobu@protonmail.com>
2023-06-01 17:42:24 +0600

    array pop and push test

---
+++ Arrays/index.js
@@ -0,0 +1,26 @@
+// let myExperiences = [
+//     "Technical Specialist at UNDP Kyrgyzstan",
+//     "Engineering Specialist at PBC of the Kyrgyz Republic",
+//     "Engineer Intern at BTU",
+//     "Engineer Intern at Severelectro"
+// ]
+//
+
+// let describeMyself = [
+    // "Amantur",
+    // 24,
+    // true
+// ]
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.push(newMessage);
+console.log(messages);
+
+messages.pop(newMessage);
+console.log(messages);
